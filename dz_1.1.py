2a486f7619df45d375ae401c9e03cb9b7e4355f5
nojio4ka <78817271+nojio4ka@users.noreply.github.com>
2021-02-28 20:51:48 +0300

    Add files via upload

---
+++ dz_1.1.py
@@ -0,0 +1,9 @@
+number = 10
+text = 'Hello'
+print(number, text)
+user_number1 = input('Введите первое число: ')
+user_number2 = input('Введите второе число: ')
+print('Первое число - ', user_number1, 'Второе число - ', user_number2)
+name = input('Введите имя: ')
+surname = input('Введите фамилию: ')
+print(name, surname)
